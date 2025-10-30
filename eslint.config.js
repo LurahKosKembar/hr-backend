0e56fc701a3155a4be2a9a72913c537bda9dc9c7
titanaprilian <titanaprilian73@gmail.com>
2025-10-30 13:12:57 +0700

    chore: normalize eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,27 @@
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsparser from "@typescript-eslint/parser";
-import prettierPlugin from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
   {
-    rules: {
-      ...tseslint.configs.recommended.rules,
-      ...prettierConfig.rules,
-      "@typescript-eslint/no-unused-vars": "warn",
-      "no-console": "warn",
-      semi: ["error", "always"],
-      quotes: ["error", "double"],
-      "prettier/prettier": "error",
+    files: ['**/*.ts'],
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettierPlugin,
     },
-
-    files: ["**/*.ts"],
-
+    extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-console': 'warn',
+      semi: ['error', 'always'],
+      quotes: ['error', 'double'],
+      'prettier/prettier': 'error',
+      'no-tabs': 'error',
+      indent: ['error', 2, { SwitchCase: 1 }],
+    },
     languageOptions: {
       parser: tsparser,
-      sourceType: "module",
-    },
-
-    plugins: {
-      "@typescript-eslint": tseslint,
-      prettier: prettierPlugin,
+      sourceType: 'module',
     },
   },
 ];
